425c254a1513cbfd8fba527cf0339846371862f4
Joe Sandmeyer <github.com@unconscious.com>
2023-05-24 18:54:08 -0400

    copied from muheli/vsp-hud-message

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# overlay-message
+# Overlay Message 🔌
+
+This open source plugin is designed to be used from within [Vatom Spaces](https://vatom.com).
+
+## Try It
+You can find this plugin in the Vatom Market at
+- [market.vatom.com/product/vsp-hud-message](https://market.vatom.com/product/vsp-hud-message/)
+
+## Development
+
+- Ensure you have [Node](https://nodejs.org) installed
+- Install dependencies with `npm install`
+- Login to the Vatom CLI with `npm run login`
+- Build and load the plugin into your space with `npm run sideload -- myspace` (replace `myspace` with your space alias name)
+- When ready, publish to the Marketplace with `npm run publish`
+
+> **Note:** You can only sideload plugins in a space you are the owner of.
+
+
+http://localhost:9000/plugin.js
---
+++ src/index.js
@@ -0,0 +1,38 @@
+import { BasePlugin, BaseComponent } from 'vatom-spaces-plugins'
+
+/**
+ * This is the main entry point for your plugin.
+ *
+ * All information regarding plugin development can be found at
+ * https://developer.vatom.com/plugins/plugins/
+ *
+ * @license MIT
+ * @author Vatom Inc.
+ */
+export default class MyPlugin extends BasePlugin {
+
+    /** Plugin info */
+    static id = "vatom-template-plugin"
+    static name = "Vatom Template Plugin"
+
+    /** Called on load */
+    onLoad() {
+
+        // Create a button in the toolbar
+        this.menus.register({
+            icon: this.paths.absolute('button-icon.png'),
+            text: 'Plugin',
+            action: () => this.onButtonPress()
+        })
+
+    }
+
+    /** Called when the user presses the action button */
+    onButtonPress() {
+
+        // Show alert
+        this.menus.alert(`Hello from ${this.constructor.name} version ${require('../package.json').version}!`, 'Hello world!', 'info')
+
+    }
+
+}
